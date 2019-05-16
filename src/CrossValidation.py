3f0b6095be5269bba704d66ddff4867e8ec81fbd
Hugo Constantinopolos <hugo.cpolos@gmail.com>
2019-05-16 17:39:18 -0300

    Fold Build tested and working

--- src/CrossValidation.py
+++ src/CrossValidation.py
@@ -1,3 +1,6 @@
+import random
+
+
 class CrossValidation(object):
     """docstring for CrossValidation"""
 
@@ -8,31 +11,86 @@ class CrossValidation(object):
         self.folds = []
         self.possible_classes = list(dict.fromkeys(
             [d[predictclass] for d in data]))
+        self.generate_data_for_each_class()
+        self.count_classes()
+
+    def generate_data_for_each_class(self):
         self.data_per_class = {}
         for class_ in self.possible_classes:
             self.data_per_class[class_] = [
-                c for c in self.data if c[predictclass] == class_]
+                c for c in self.data if c[self.predictclass] == class_]
+
+    def count_classes(self):
+        class_counter = {}
+
+        for d in self.data:
+            try:
+                class_counter[d[self.predictclass]] += 1
+            except KeyError:
+                class_counter[d[self.predictclass]] = 1
+
+        self.class_counter = class_counter
 
     def generate_stratified_folds(self, k=10):
-        # for fold in k:
-        _temp_data_ = []
-        max_len = 0
-        for d in self.data_per_class:
-            max_len = (len(self.data_per_class[d])) if (
-                len(self.data_per_class[d])) > max_len else max_len
 
-        for i in range(max_len):
-            for d in self.data_per_class:
+        self.calculate_proportion(k)
+
+        for i in range(k):
+            new_fold = []
+
+            for class_ in self.class_counter:
+                for j in range(self.proportion[class_]['each_fold']):
+                    new_sample = random.choice(self.data_per_class[class_])
+                    self.data_per_class[class_].remove(new_sample)
+                    new_fold.append(new_sample)
+            self.folds.append(new_fold)
+
+        # randomly choice the extra samples
+        while(len(self.extra_samples) > 0):
+            for fold in self.folds:
                 try:
-                    n = self.data_per_class[d][i]
-                    _temp_data_.append(n)
-                except:
+                    new_sample = random.choice(self.extra_samples)
+                    self.extra_samples.remove(new_sample)
+                    fold.append(new_sample)
+                except IndexError:
                     pass
 
-        self.folds = split(_temp_data_, k)
+    def calculate_proportion(self, k):
+        # Calculate the classes proportion for a given k
+        proportion = {}
+        for class_ in self.class_counter:
+            div, rest = divmod(self.class_counter[class_], k)
+            proportion[class_] = {}
+            proportion[class_]['each_fold'] = div
+            proportion[class_]['extra'] = rest
+
+        self.proportion = proportion
+        self.create_extra_samples()
+
+    def create_extra_samples(self):
+        extra_samples = []
+        for class_ in self.class_counter:
+            for j in range(self.proportion[class_]['extra']):
+                new_sample = random.choice(self.data_per_class[class_])
+                self.data_per_class[class_].remove(new_sample)
+                extra_samples.append(new_sample)
+        self.extra_samples = extra_samples
 
 
 def split(a, n):
     # https://stackoverflow.com/questions/2130016/splitting-a-list-into-n-parts-of-approximately-equal-length
     k, m = divmod(len(a), n)
     return [a[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n)]
+
+
+if __name__ == '__main__':
+    data = [{'i': '1', '.': 'a'}, {'i': '2', '.': 'a'},
+            {'i': '3', '.': 'a'}, {'i': '4', '.': 'b'}, {'i': '5', '.': 'b'},
+            {'i': '6', '.': 'b'}, {'i': '7', '.': 'c'}, {'i': '8', '.': 'c'},
+            {'i': '9', '.': 'c'}, {'i': '10', '.': 'd'}]
+    possible_classes = ['a', 'b', 'c', 'd']
+    c = CrossValidation(data, '.')
+    c.generate_stratified_folds(3)
+    print('Folds:')
+    for set_ in c.folds:
+        print (set_)
